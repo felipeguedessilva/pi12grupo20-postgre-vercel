fe5fcc02cc943c39e477c9e6b854b7b999d8fa7a
grupo20pi2univesp <grupo20pi2univesp@gmail.com>
2024-10-21 14:25:41 -0300

    C25

--- frameworkweb_projeto/urls.py
+++ frameworkweb_projeto/urls.py
@@ -22,7 +22,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('polls.urls')),
-]
+];
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
